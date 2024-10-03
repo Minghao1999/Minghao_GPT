e998250ddd9b31bc379236e529eb4efe5f2ba4d8
Minghao1999 <m_sun11@u.pacific.edu>
2024-10-02 22:19:26 -0700

    Ignore config.py

--- backend/config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    OPENAI_API_KEY = "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"
-    MONGO_URI = "mongodb+srv://sun989minghao:bBirnN44tnmAUzFq@cluster0.4uiwt.mongodb.net/DC_GPT?retryWrites=true&w=majority&appName=Cluster0"
