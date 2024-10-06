99dc7aed1907ef6dab97a0bec6f1f9c9d7de9f04
Minghao1999 <m_sun11@u.pacific.edu>
2024-10-05 23:57:33 -0700

    commit

---
+++ frontend/src/Components/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div className="landing-container">
+            <h1>Welcome to Minghao GPT!</h1>
+            <p>You can ask everything you want to know about Minghao.</p>
+        </div>
+    );
+};
+
+export default Landing;
