b589e9f1b6071663538a2870ae83635e9db7d0c8
Minghao1999 <m_sun11@u.pacific.edu>
2024-10-16 21:32:40 -0700

    add issue explain

--- frontend/src/Components/Message/Landing.jsx
+++ frontend/src/Components/Message/Landing.jsx
@@ -7,6 +7,12 @@ const Landing = ({onCardClick}) => {
             <div className="landing-content">
             <h1>Welcome to Minghao GPT!</h1>
             <p>You can ask everything you want to know about Minghao.</p>
+                <div className="issue">
+                    <p>Minghao GPT is working in my local environment</p>
+                    <p>Cause the browser is using HTTPS but the server is using HTTP which is the insecure HTTP protocol.</p>
+                    <p>So, due to the browser's security mechanism, it blocks this request.</p>
+                    <p>I'm trying to configure Flask to support HTTPS.</p>
+                </div>
             </div>
 
             <div className="card-container">
